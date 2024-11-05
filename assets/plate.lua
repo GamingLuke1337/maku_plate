40ea2daab2defd1aca8e1d922f2a3f842df7c9d1
Erzik1 <118635278+Erzik1@users.noreply.github.com>
2024-11-05 16:36:49 +0100

    Update plate.lua

--- assets/plate.lua
+++ assets/plate.lua
@@ -1,10 +1,18 @@
+-- Save the original function under a new name to avoid cyclic calling
+local originalGetVehicleNumberPlateText = GetVehicleNumberPlateText
+
+-- Override the GetVehicleNumberPlateText function
 GetVehicleNumberPlateText = function(vehicle)
-    local plate = GetVehicleNumberPlateText(vehicle)
+    -- Get the license plate text from the original function
+    local plate = originalGetVehicleNumberPlateText(vehicle)
+    
+    -- If the license plate is empty, load the value from the state bag
     if plate:gsub('%s+', '') == '' then
         local statebag = Entity(vehicle).state.plate
         if statebag ~= nil then
             plate = statebag
         end
     end
+
     return plate
 end
