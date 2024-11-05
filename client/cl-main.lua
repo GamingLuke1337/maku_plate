b474c49afe4c0c4d151b45942dddf1a50b94eea6
Erzik1 <118635278+Erzik1@users.noreply.github.com>
2024-11-05 16:35:44 +0100

    Update cl-main.lua

--- client/cl-main.lua
+++ client/cl-main.lua
@@ -1,21 +1,23 @@
 local inventory = exports.ox_inventory
 
+-- Function to find the closest vehicle
 function getClosestVehicleToPlayer(plyPos, radius)
     local retval, statusCode = nil, 'unk'
-
-    if GetClosestVehicle(plyPos.x, plyPos.y, plyPos.z, radius and radius or CLOSEST_VEHICLE_RANGE, 0, 23) then
-        retval = GetClosestVehicle(plyPos.x, plyPos.y, plyPos.z, radius and radius or CLOSEST_VEHICLE_RANGE, 0, 23)
+    local vehicle = GetClosestVehicle(plyPos.x, plyPos.y, plyPos.z, radius or CLOSEST_VEHICLE_RANGE, 0, 23)
+    if vehicle then
+        retval = vehicle
         statusCode = 'found_vehicle'
     end
-
     return retval, statusCode
 end
 
+-- Check if resource is active
 function isResourcePresent(resourceName)
     local state = GetResourceState(resourceName)
     return state == 'started' or state == 'starting'
 end
 
+-- Remove license plate
 local function takeOff(vehicle)
     local netId = NetworkGetNetworkIdFromEntity(vehicle)
     TriggerServerEvent('maku_plate:server:takeoff', netId)
@@ -24,6 +26,7 @@ end
 AddEventHandler('maku_plate:client:takeoff', takeOff)
 exports('takeOff', takeOff)
 
+-- Attach license plate
 local function putOn(vehicle, plate)
     local netId = NetworkGetNetworkIdFromEntity(vehicle)
     TriggerServerEvent('maku_plate:server:puton', netId, plate)
@@ -32,6 +35,7 @@ end
 AddEventHandler('maku_plate:client:puton', putOn)
 exports('putOn', putOn)
 
+-- Item usage for attaching license plate
 exports('itemUsage', function(data, slot)
     local vehicle, statusCode = getClosestVehicleToPlayer(GetEntityCoords(PlayerPedId()))
     if DoesEntityExist(vehicle) then
@@ -43,19 +47,20 @@ exports('itemUsage', function(data, slot)
     end
 end)
 
+-- Set ox_target for removing license plate
 if isResourcePresent('ox_target') then
     local target = exports.ox_target
 
     Citizen.CreateThread(function()
         target:addGlobalVehicle({
             {
-                label = TARGET_TAKE_OFF,
-                name = 'takeoffplate',
-                icon = 'fa-solid fa-tarp',
-                distance = 3.0,
+                label = "Take off plate", -- Button text
+                name = 'takeoffplate', -- Action name
+                icon = 'fa-solid fa-tarp', -- Icon
+                distance = 3.0, -- Maximum distance
                 canInteract = function(vehicle, distance, coords, name, bone)
                     local plate = GetVehicleNumberPlateText(vehicle)
-                    return plate ~= '' and plate ~= EMPTY_PLATE
+                    return plate ~= '        ' and plate ~= 'NO_PLATE'
                 end,
                 onSelect = function(data)
                     takeOff(data.entity)
@@ -64,7 +69,8 @@ if isResourcePresent('ox_target') then
         })
     end)
 
-    AddEventHandler('onResourceStop', function(resoure)
+    -- Remove action when resource stops
+    AddEventHandler('onResourceStop', function(resource)
         if resource ~= GetCurrentResourceName() then return end
         target:removeGlobalVehicle('takeoffplate')
     end)
@@ -73,9 +79,7 @@ else
         local plyPed = PlayerPedId()
         local plyPos = GetEntityCoords(plyPed)
         local vehicle, statusCode = getClosestVehicleToPlayer(plyPos)
-
         if DoesEntityExist(vehicle) then
-            local plate = GetVehicleNumberPlateText(vehicle)
             takeOff(vehicle)
         else
             print('^1[error]^0 no vehicle found (command)')
